b1b099274d8efd0aca76fbb9d777e2376ab52bfb
commsagent-cmyk <comms.agent@larocquepm.com>
2025-10-17 11:37:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def home():
   return "✅ LPM Realtime server is alive."
 
 if __name__ == "__main__":
-    # Render will set PORT for us later; this is fine for local.
     import os
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
